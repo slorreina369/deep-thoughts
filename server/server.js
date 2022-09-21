d013ecd6e26807f003e9186ed9a0d68a3cc1e892
lorreina <slorreina369@gmail.com>
2022-09-20 17:11:48 -0700

    back end is displayed on front end

--- server/server.js
+++ server/server.js
@@ -19,13 +19,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(_dirname, '../client/build')));
-};
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(_dirname, '../client/build/index.html'));
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startAppoloServer = async (typeDefs, resolvers) =>{
   await server.start();
