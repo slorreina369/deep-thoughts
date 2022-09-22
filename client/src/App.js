eb4befe178ed7dfa277ec266bd8d98f7fcd4745d
lorreina <slorreina369@gmail.com>
2022-09-22 09:50:19 -0700

    created front end authentication

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -15,9 +16,19 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers:{
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link:httpLink,
-  cache: new InMemoryCache()
+  link:authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
